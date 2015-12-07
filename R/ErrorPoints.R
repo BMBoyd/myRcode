4d6168c3f95139eab537b0cdb2efc72711e48408
lsu2010 <bboyd4ster@gmail.com>
2015-12-07 16:36:57 -0500

    Improve functions

--- R/ErrorPoints.R
+++ R/ErrorPoints.R
@@ -6,11 +6,25 @@
 #' @param y.unc a vector of uncertainites added and subracted from y to create segments.
 #' @param show.hor logical; if TRUE, function draws horizontal segments
 #' @param show.ver logical; if TRUE, function draws vertical segments
+#' @param xlog logical; if TRUE changes nonpositive values to xlog.value
+#' @param xlog.value default 0.01
+# make this a log = x y or xy in future
+#' @param na.rm logical; if TRUE, NA values in x and x.unc (y and y.unc) values result in no vertical (horizontal) line being drawn.
 #' @param ... further arguments passed to segments
 #' @export ErrorPoints
-ErrorPoints<- function(x = NA, x.unc = NA, y = NA, y.unc = NA ,show.hor = T, show.ver = T, ...){
+ErrorPoints<- function(x = NA, x.unc = NA, y = NA, y.unc = NA, show.hor = TRUE, show.ver = TRUE, xlog = FALSE, xlog.value = 0.01, na.rm = TRUE, ...){
+  if( na.rm){
+    y[is.na(x) | is.na(x.unc)] <- NA
+    x[is.na(y) | is.na(y.unc)] <- NA
+  }
   if( show.hor){
-    segments(x - x.unc, y , x + x.unc, y, ...)
+    if( !xlog){
+      segments(x - x.unc, y , x + x.unc, y, ...)
+    } else {
+      xlo <- x - x.unc
+      xlo[x - x.unc <= 0] <- xlog.value
+      segments( xlo, y, x + x.unc, y, ...)
+    }
   }
   if( show.ver){
     segments(x, y - y.unc, x, y + y.unc, ...)
--- R/xsPb210.R
+++ R/xsPb210.R
@@ -1,21 +1,30 @@
 #' Calculate Excess Pb-210
 #'
+#' @export
+#' @param PbAct vector of total Pb-210 activity values
+#' @param PbActUnc vector of total Pb-210 activity error. Note: absolute not percent
+#' @param SupPbAct vector or single value to be used for supported Pb-210
+#' @param SupPbActUnc vector or single value of supported Pb-210 activity error. Note: absolute not percent
 #'
 #' @return Returns a two column vector of numerics with names xs210Pb and xs210Pb.uncer. NAs are placed where data is missing. If show.zero is true (default), then zeros are printed where xsPb210 is determined to be zero.
-xsPb210 <- function(){
-xs210Pb <- apply(njsg[, c( "X47_Pb210_activity", "X609_Bi214_activity", "X47_Pb210_error")], 1, function(x){
-  if(x[1]-x[2] >= 0 & !is.na(x[3])){
-    x[1]-x[2]
-  }else{
-    NA
+xsPb210 <- function(PbAct = NA, PbActUnc = NA, SupPbAct = NA, SupPbActUnc = NA){
+  xs210Pb <- apply( c( PbAct, SupPbAct, PbActUnc), 1, function(x){
+    if(x[1]-x[2] >= 0 & !is.na(x[3])){
+      x[1]-x[2]
+    }else{
+      NA
+    }
   }
-})
-xs210Pb.uncer <- apply( njsg[, c( "X47_Pb210_error", "X609_Bi214_error", "xs210Pb")], 1, function(x) {
-  if(is.na(x[3])){
-    NA
-  }else{
-    x[1] + x[2]
+  )
+
+  xs210Pb.uncer <- apply( c( PbActUnc, SupPbActUnc, xs210Pb), 1, function(x) {
+    if(is.na(x[3])){
+      NA
+    }else{
+      x[1] + x[2]
+    }
   }
-})
-data.frame( xs210Pb = xs210Pb, xs210.uncer = xs210Pb.uncer)
+  )
+
+  data.frame( xs210Pb = xs210Pb, xs210.uncer = xs210Pb.uncer)
 }
