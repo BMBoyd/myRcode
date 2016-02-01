e15b51bcb69a47db8fcbea7e4170b667f308e9d1
lsu2010 <bboyd4ster@gmail.com>
2016-02-01 16:30:51 -0500

    make myaxis smarter for log axes

--- R/myaxis.R
+++ R/myaxis.R
@@ -5,10 +5,17 @@
 #' @param append.label a character string with which to append the axis tick label, ex. "\%"
 #' @export
 
-myaxis <- function( side, label.line = 0.75, append.label = "", col = "black"){
+myaxis <- function(side, label.line = 0.75, append.label = "", col = "black"){
 
-  axis(side = side, at=axTicks(side), labels=F, col = col)
-
-mtext(text = paste(axTicks(side), append.label, sep = ""), side = side, line = label.line, at=axTicks(side), las = 1, col = col)
+  ticks <- axTicks(side)
+  axis(side = side, at = ticks, labels = F, col = col)
+  # constrain writing of labels when plotting in log scale
+if( side %in% c(1,3) & par("xlog")){
+  ticks <- ticks[ ticks >= 10^ min(par("usr")[1:2]) & ticks <= 10^max(par("usr")[1:2])]
+}
+  if( side %in% c(2,4) & par("ylog")){
+    ticks <- ticks[ ticks <= 10^ min(par("usr")[3:4]) & ticks <= 10^max(par("usr")[3:4])]
+  }
+mtext(text = paste(ticks, append.label, sep = ""), side = side, line = label.line, at = ticks, las = 1, col = col)
 
 }
