63c8c026b9acf001e4d9f964089a2dd1364777c6
lsu2010 <bboyd4ster@gmail.com>
2016-02-05 10:55:19 -0500

    Update errorrange with xlog args

--- R/errorrange.R
+++ R/errorrange.R
@@ -2,11 +2,15 @@
 #'
 #' @param x numeric vector
 #' @param x.unc numeric vector of uncertainties same length as x
+#' @param xlog logical; if TRUE changes nonpositive values to xlog.value
+#' @param xlog.value default 0.01
 #' @param na.rm remove na? default TRUE
 #' @export
 
-errorrange <- function( x, x.unc = 0.75, na.rm = T){
-
-  range(c(range(x + x.unc, na.rm = na.rm),  range(x - x.unc, na.rm = na.rm)), na.rm = na.rm)
-
+errorrange <- function(x, x.unc , xlog = F, xlog.value = 0.01, na.rm = T){
+  if ( missing(x) | missing(x.unc)) stop("x or x.unc missing with no default")
+  x.unc <- abs(x.unc)
+  x.lo <- x - x.unc
+  if (xlog)  x.lo[ x.lo <= 0] <- xlog.value
+  range(c(range(x + x.unc, na.rm = na.rm),  range(x.lo, na.rm = na.rm)), na.rm = na.rm)
 }
